6d30ff75baff69c227d074f27771b2a0b023f869
Porkodi Mahendran <mahendpo@wm.ibm.com>
2018-08-14 10:49:45 +0800

    incremental update

--- zuul-service/src/main/java/com/techprimers/stock/zuulservice/WebSecurityConfig.java
+++ zuul-service/src/main/java/com/techprimers/stock/zuulservice/WebSecurityConfig.java
@@ -11,7 +11,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         // Configure HttpSecurity as needed (e.g. enable http basic).
-    	http.authorizeRequests().antMatchers("/").permitAll();
+    	http.authorizeRequests().antMatchers("/").permitAll();    	
     	http.csrf().disable();
     }
 }
